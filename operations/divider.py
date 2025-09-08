473a50eef7fa725e8a5ad8375d83cbf518801b37
NADIA14MUDLIKAH <nadia14mudlikah@gmail.com>
2025-09-08 13:58:52 +0000

    Revert "feat: tambahkan fungsi divide_matrices tanpa validasi dimensi (bug)"
    This reverts commit f5b893cc0e04862a6aad56402e7b1df6b6c71a84.

--- operations/divider.py
+++
@@ -1,6 +0,0 @@
-def divide_matrices(matrix1, matrix2):
-    # BUG: Tidak ada validasi dimensi
-    result_data = [[matrix1.data[i][j] / matrix2.data[i][j]
-                    for j in range(matrix1.cols)]
-                    for i in range(matrix1.rows)]
-    return type(matrix1)(result_data)
